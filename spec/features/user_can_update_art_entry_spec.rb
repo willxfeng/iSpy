67dcd385397d0ab25343463b6a23e433ba6e61c4
jrpespisa <jrpespisa@verizon.net>
2016-01-05 13:53:00 -0500

    fixed spacing issues

--- spec/features/user_can_update_art_entry_spec.rb
+++ spec/features/user_can_update_art_entry_spec.rb
@@ -1,12 +1,12 @@
 feature "User updates art entry" do
   let(:art) do
     Art.create(
-    name: "Chinatown Community Mural",
-    location: "Surface Rd & Beach St",
-    description: "Chinese community mural commissioned
-    by the mayor's mural crew.",
-    artist: "Made up Artist",
-    category: "Graffiti"
+      name: "Chinatown Community Mural",
+      location: "Surface Rd & Beach St",
+      description: "Chinese community mural commissioned
+      by the mayor's mural crew.",
+      artist: "Made up Artist",
+      category: "Graffiti"
     )
   end
   scenario "user visits update page" do
@@ -18,7 +18,7 @@ feature "User updates art entry" do
   scenario "user successfully fills out update form" do
     visit "/arts/#{art.id}"
     click_on "Edit this Entry"
-    fill_in  "art_artist", with: "Another Artist"
+    fill_in "art_artist", with: "Another Artist"
     click_on "Update"
 
     expect(page).to have_content "Another"
@@ -26,7 +26,7 @@ feature "User updates art entry" do
   scenario "user unsuccessfully fills out update form" do
     visit "/arts/#{art.id}"
     click_on "Edit this Entry"
-    fill_in  "art_location", with: " "
+    fill_in "art_location", with: " "
     click_on "Update"
 
     expect(page).to have_content "You must fill out all of the required fields"
--- spec/features/user_views_index_page_spec.rb
+++ spec/features/user_views_index_page_spec.rb
@@ -1,12 +1,11 @@
 require 'rails_helper'
-
 feature "Visitors see index page" do
   scenario "Visitors see street art names" do
     visit "/"
 
     expect(page).to have_content "Welcome to iSpy"
   end
-  
+
   scenario "Users can add their own art" do
     visit "/"
     click_on "Add New Street Art"
