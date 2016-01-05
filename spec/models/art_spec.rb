86a4aa8e4f701f2cf8e5b781431832dcb538976b
jrpespisa <jrpespisa@verizon.net>
2016-01-05 13:18:58 -0500

    fixed additional spacing issues

--- spec/models/art_spec.rb
+++ spec/models/art_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 describe Art do
-  it { should have_valid(:name).when("Nice Mural", "Broadway St. Tag")}
-  it { should_not have_valid(:name).when(nil, "")}
+  it { should have_valid(:name).when("Nice Mural", "Broadway St. Tag") }
+  it { should_not have_valid(:name).when(nil, "") }
 
-  it { should have_valid(:location).when("Park St.", "Phoenix, AZ")}
-  it { should_not have_valid(:location).when(nil, "")}
+  it { should have_valid(:location).when("Park St.", "Phoenix, AZ") }
+  it { should_not have_valid(:location).when(nil, "") }
 
-  it { should have_valid(:description).when("Wow that's really nice")}
-  it { should_not have_valid(:description).when(nil, "")}
+  it { should have_valid(:description).when("Wow that's really nice") }
+  it { should_not have_valid(:description).when(nil, "") }
 
-  it { should have_valid(:category).when("Mural", "Graffiti")}
-  it { should_not have_valid(:category).when(nil, "")}
+  it { should have_valid(:category).when("Mural", "Graffiti") }
+  it { should_not have_valid(:category).when(nil, "") }
 end
