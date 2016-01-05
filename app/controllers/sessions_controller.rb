7152dd66f9875cd8b6178d38fafe406c65876a78
lilybarrett <lily@barrett.us>
2016-01-05 14:15:56 -0500

    devise controller default path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < Devise::SessionsController
   protected
 
-  def after_sign_in_path_for(_resource)
-    :edit_user_registration
-  end
+  # def after_sign_in_path_for(_resource)
+  #   :edit_user_registration
+  # end
 end
