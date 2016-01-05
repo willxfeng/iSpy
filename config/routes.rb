127b620dae6e59203011d0a35b3813c94fe54eb5
jrpespisa <jrpespisa@verizon.net>
2016-01-05 14:44:40 -0500

    modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root "arts#index"
   resources :arts
-  resources :arts, only: [:create, :new, :show]
   devise_for :users, controllers: {
     registrations: "registrations",
     sessions: "sessions"
