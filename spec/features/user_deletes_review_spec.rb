3fd35239720c67ec1714c14990c10033be775c10
lilybarrett <lily@barrett.us>
2016-01-11 16:48:35 -0500

    fixed hound errors

--- spec/features/user_deletes_review_spec.rb
+++ spec/features/user_deletes_review_spec.rb
@@ -6,21 +6,21 @@ feature "User deletes review" do
     artwork = FactoryGirl.create(:art)
     review = FactoryGirl.create(:review, art_id: artwork.id, user_id: user.id)
 
-     visit root_path
-     click_link "Sign In!"
-     fill_in 'Email', with: user.email
-     fill_in 'Password', with: user.password
+    visit root_path
+    click_link "Sign In!"
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
 
-     click_button 'Sign In'
-     visit art_path(artwork)
+    click_button 'Sign In'
+    visit art_path(artwork)
 
-     click_on "Delete this Review"
+    click_on "Delete this Review"
 
-     expect(page).to have_content "Chinatown"
-     expect(page).to_not have_content "greatest mural"
+    expect(page).to have_content "Chinatown"
+    expect(page).to_not have_content "greatest mural"
   end
 
-  scenario "User cannot delete review if his or her user id is not associated with the artwork" do
+  scenario "User cannot delete review if not user's review" do
     user = FactoryGirl.create(:user)
     artwork = FactoryGirl.create(:art)
     review = FactoryGirl.create(:review, art: artwork)
