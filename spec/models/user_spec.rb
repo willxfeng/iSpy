ca05ccffbd372ec0a39813480b8d87d66c8e206f
Will Feng <willxfeng@gmail.com>
2016-01-12 16:27:57 -0500

    user#admin? unit test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,13 +25,22 @@ describe User do
     expect(user.errors[:password_confirmation]).to_not be_blank
   end
 
+  let(:user) { User.new(first_name: 'Big', last_name: 'Bird') }
+
   context "#full_name" do
     it "returns full name" do
-      user = User.new(
-        first_name: 'Big',
-        last_name: 'Bird')
-
       expect(user.full_name).to eq('Big Bird')
+    end
+  end
+
+  context "#admin?" do
+    it "returns true for admin" do
+      user.role = 'admin'
+      expect(user.admin?).to be true
+    end
+
+    it "returns false for member" do
+      expect(user.admin?).to be false
     end
   end
 end
