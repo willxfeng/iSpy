fee03f9e3bca68a7f8bec26746f084ea4b72c470
jrpespisa <jrpespisa@verizon.net>
2016-01-13 14:46:13 -0500

    final changes made

--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -1,6 +1,6 @@
 Kaminari.configure do |config|
-  config.default_per_page = 1
-  config.max_per_page = 1
+  config.default_per_page = 5
+  config.max_per_page = 5
   # config.window = 4
   # config.outer_window = 0
   # config.left = 0
