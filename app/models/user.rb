efaa273c88337ab5fe2004acc0d53f305b1ac01c
Will Feng <willxfeng@gmail.com>
2016-01-15 10:54:03 -0500

    hound violation

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
-  
+
   devise :database_authenticatable,
     :registerable,
     :recoverable,
